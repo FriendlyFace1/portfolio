dffc17473b523bfcff574b652a51c94a43de2a47
suegavin <iamgavinsue@gmail.com>
2019-12-02 23:16:38 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+  <h1>Gavin Sue's Engineering Portfolio</h1>
+    <?php echo '<p>Hello World! Here are all of my Links Gavin Sue Period 7</p>'; ?> 
+    <ul>
+      <li><a href="index.html">Home</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Sue_AboutMe.html">About Me</a></li></br>
+      <li><a href="Sue_Projects.html">Projects</a></li></br>
+      <li><a href="Sue_Resume.html">Resume</a></li></br>
+      <li><a href="Sue_ContactMe.html">Contact Me</a></li></br>
+    <ul>
+  </body>
+</html>
